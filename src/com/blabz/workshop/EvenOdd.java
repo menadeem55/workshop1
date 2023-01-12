be93f6b5bddc4c2940df0d8260f790a91d878e73
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2023-01-12 09:28:22 +0530

    Added: workshop

---
+++ src/com/blabz/workshop/EvenOdd.java
@@ -0,0 +1,27 @@
+package com.blabz.workshop;
+
+//345328 ==>  3+5+3     4+2+8
+
+public class EvenOdd {
+
+	public static void odd() {
+
+	}
+
+	public static void even() {
+
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println("welcome to Even Odd program");
+
+		int num = 345328;
+
+		if (num % 2 == 0) {
+			System.out.println("Even");
+		}
+
+	}
+
+}
---
+++ src/com/blabz/workshop/StringArray.java
@@ -0,0 +1,26 @@
+package com.blabz.workshop;
+
+import java.util.Arrays;
+
+public class StringArray {
+	
+	public static void main(String[] args) {
+	
+		
+		String fruits[] = new String[3];
+		
+		fruits[0] = "Apple";
+		fruits[1] = "bnana";
+		fruits[2] = "Orange";
+		
+		System.out.println("Original String: " + Arrays.toString(fruits));
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
